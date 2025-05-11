82786dfb6f156866a687e7098ae6097e31763696
Muralidharan <muralimuthu047@gmail.com>
2025-05-11 14:47:49 +0530

    Added VARIABLES_AND_DATATYPES.py file

---
+++ VARIABLES_AND_DATATYPES.py
@@ -0,0 +1,10 @@
+# PERSONAL INFO COLLECTOR
+
+name = input("Enter your name : ")
+age = input("How old are you? : ")
+isStudent = input("Are you a student? (Yes/No) : ")
+
+print("\nPERSONAL INFO\n")
+print("NAME : ",name)
+print("AGE : ",age)
+print("IS_STUDENT : ",isStudent)
