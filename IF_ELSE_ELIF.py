17dd56560a0558dc8f18a181a98590f1dd690346
Muralidharan <muralimuthu047@gmail.com>
2025-05-21 11:24:19 +0530

    Added Mini Project for if-elif-else statement

---
+++ IF_ELSE_ELIF.py
@@ -0,0 +1,13 @@
+# Mini Project
+
+weather = "rainy"
+
+temperature = 25
+
+if weather == "rainy":
+    if temperature < 20:
+        print("Wear a raincoat and sweater")
+    else:
+        print("Just carry an umbrella")
+else:
+    print("No rain today!")
